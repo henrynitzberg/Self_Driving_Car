a874db5936a6028c7076690463fa84c742f281fd
Henry Nitzberg <henry@nitzb.org>
2024-04-01 09:23:22 -0400

    outline for class to gray value

---
+++ misc_class_stuffs/gray_class_values.py
@@ -0,0 +1,9 @@
+import cv2
+import numpy as np
+
+# reads camvid classes to color map
+# reads camvid color to PfD: Ground Truth color (?) map
+
+# create PfD to gray map
+
+# determine class to gray map (by doing PdD_to_gray[camvid_color_to_PfD_color[camvid_class_to_color["person"]]])
