c61a6c30bbd9457a3a76fb487c818ab7296586ea
Henry Nitzberg <nitz@Henrys-Windows-Computer.local>
2024-04-27 16:59:30 -0400

    user friendly prediction functions

--- Segmentation/predict_segment.py
+++ Segmentation/predict_segment.py
@@ -6,22 +6,12 @@ import torch
 import torchvision.transforms as tf
 import matplotlib.pyplot as plt
 
-# picks random image in the first X data to segment
-X = 2500
-randnum = np.random.randint(1, X)
-randnum = 1
 
 model_path = os.path.abspath("../models/segmentation_model_6class.pt")
-image_path = os.path.abspath(f"../data/01_images/{randnum:05d}.png")
 
 IMAGE_DIMS = (950, 500)
 
-image = cv2.imread(image_path)
-
-image = cv2.resize(image, IMAGE_DIMS, interpolation=cv2.INTER_NEAREST)
-
 transformImg = tf.Compose([tf.ToPILImage(),tf.ToTensor(),tf.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
-
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu') 
 Net = torchvision.models.segmentation.deeplabv3_resnet50(weights='DEFAULT')
 Net.classifier[4] = torch.nn.Conv2d(256, 6, 1)
@@ -34,17 +24,14 @@ else:
 
 Net.eval()
 
-to_segment = transformImg(image)
-to_segment = torch.autograd.Variable(to_segment, requires_grad=False).to(device).unsqueeze(0)
-with torch.no_grad():
-    prediction = Net(to_segment)['out']  # Run net
+def predict_segment(image):
+    image = cv2.resize(image, IMAGE_DIMS, interpolation=cv2.INTER_NEAREST)
+    to_segment = transformImg(image)
+    to_segment = torch.autograd.Variable(to_segment, requires_grad=False).to(device).unsqueeze(0)
+    with torch.no_grad():
+        prediction = Net(to_segment)['out']  # Run net
 
-prediction = tf.Resize((IMAGE_DIMS[1], IMAGE_DIMS[0]))(prediction[0])
-seg = torch.argmax(prediction, 0).cpu().detach().numpy()
-cv2.imshow("original image", image)
-cv2.imshow("gray segmentation map", (seg * 50).astype(np.uint8))
+    prediction = tf.Resize((IMAGE_DIMS[1], IMAGE_DIMS[0]))(prediction[0])
+    seg = torch.argmax(prediction, 0).cpu().detach().numpy()
+    return seg
 
-plt.imshow(seg, cmap='viridis')  # visualy easier to understand
-plt.title("colorful segmentation map")
-plt.show()
-cv2.waitKey()
