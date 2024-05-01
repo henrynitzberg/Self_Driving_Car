7f5f2837621c2a38d6310a6bfc0b3f2300ca9b3c
Henry Nitzberg <henry@nitzb.org>
2024-04-30 21:54:31 -0400

    new segmentation model updates

--- Segmentation/predict_segment.py
+++ Segmentation/predict_segment.py
@@ -11,17 +11,18 @@ X = 2500
 # randnum = np.random.randint(1, X)
 randnum = 439 
 
-model_path = os.path.abspath("../models/segmentation_model_4class.pt")
-image_path = os.path.abspath(f"../data/01_images/{randnum:05d}.png")
-# image_path = os.path.abspath(f"../data/CARLA_toSeg/rgb/1714229204409982800_0.450_0.750_0.000.png")
-
+# info relevant to model (actually image dims are not fixed)
 IMAGE_DIMS = (475, 250)
 NUM_CLASSES = 5
 
-image = cv2.imread(image_path)
+model_path = os.path.abspath("../models/segmentation_model_4class.pt")
 
-image = cv2.resize(image, IMAGE_DIMS, interpolation=cv2.INTER_NEAREST)
-image = 
+# for testing
+# image_path = os.path.abspath(f"../data/01_images/{randnum:05d}.png")
+# image_path = os.path.abspath(f"../data/CARLA_toSeg/rgb/1714229204409982800_0.450_0.750_0.000.png")
+
+# image = cv2.imread(image_path)
+# image = cv2.resize(image, IMAGE_DIMS, interpolation=cv2.INTER_NEAREST)
 
 transformImg = tf.Compose([tf.ToPILImage(),tf.ToTensor(),tf.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
 device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu') 
@@ -44,9 +45,12 @@ def predict_segment(image):
     to_segment = torch.autograd.Variable(to_segment, requires_grad=False).to(device).unsqueeze(0)
     with torch.no_grad():
         prediction = Net(to_segment)['out']  # Run net
+    return prediction
 
-prediction = tf.Resize((IMAGE_DIMS[1], IMAGE_DIMS[0]))(prediction[0])
-seg = torch.argmax(prediction, 0).cpu().detach().numpy()
-cv2.imshow("original image", image)
-cv2.imshow("gray segmentation map", (seg * 30).astype(np.uint8))
-
+# for testing
+# prediction = predict_segment(image)
+# prediction = tf.Resize((IMAGE_DIMS[1], IMAGE_DIMS[0]))(prediction[0])
+# seg = torch.argmax(prediction, 0).cpu().detach().numpy()
+# cv2.imshow("original image", image)
+# cv2.imshow("gray segmentation map", (seg * 30).astype(np.uint8))
+# cv2.waitKey()
